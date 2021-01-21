fab966914743bc90b5973a13d7af343e14c3668c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-21 15:56:40 -0500

    commit tree b3dba0379a2b650985d069038d207fa00d75aba8

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,21 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+ 
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+    BRANDS << brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+
 
 end
